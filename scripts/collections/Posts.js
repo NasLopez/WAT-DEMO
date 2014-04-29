68a0940160487073e7de600b035bc6cdb1d126b8
Nasser Lopez <nasserlopez@gmail.com>
2014-04-29 15:58:14 -0400

    added a filter by source feature

--- scripts/collections/Posts.js
+++ scripts/collections/Posts.js
@@ -2,7 +2,26 @@
 define(['jquery','underscore','backbone'], function($, _, Backbone){
 
 	var posts = Backbone.Collection.extend({
-		url: 'api/posts.json'
+		url: 'api/posts.json',
+		sources: function () {
+			var sources = [];
+			
+			_.each(this.models[0].attributes.posts, function(post) {
+				if (!_.contains(sources, post.source))
+				{
+					sources.push(post.source);
+				}
+
+			});
+
+			return sources;
+		},
+		filterSourceBy: function (by) {
+			if (by != 'all'){
+				return _.where(this.models[0].attributes.posts, {source: by });
+			} 
+			return  this.models[0].attributes.posts;
+		}
 	});
 
 	return posts;
--- scripts/views/HomeView.js
+++ scripts/views/HomeView.js
@@ -1,20 +1,29 @@
 //File: views/HomeView.js
-define(['jquery','underscore','backbone', 'collections/Posts', 'text!templates/homeTemplate.html'], function($, _, Backbone, Posts, homeTemplate){
+define(['jquery','underscore','backbone', 'collections/Posts', 'text!templates/home/homeTemplate.html', 'text!templates/home/postsListTemplate.html'], function($, _, Backbone, Posts, homeTemplate, postList){
 
 	var HomeView = Backbone.View.extend({
 		el: '.page',
-		render:function () {
+		render: function () {
 			var that = this;
-			var posts = new Posts;
-			posts.fetch({
+			that.posts = new Posts;
+			console.log("HomeView FETCH: ", that.posts.fetch);
+			that.posts.fetch({
 				success: function (posts) {
-					console.log(posts.models[0].attributes.posts);
-					var template = _.template(homeTemplate, {posts: posts.models[0].attributes.posts});
+					var template = _.template(homeTemplate, {sources: that.posts.sources()});
 					that.$el.html(template);
+					template = _.template(postList, {posts: that.posts.filterSourceBy('all')});
+					that.$('#posts-list').html(template);
 				}
 			});
+		},
+		events: {
+			'change #ddpFilter' : 'filter'
+		},
+		filter : function(e){
+			console.log("Event", e.target.value);
+			var template = _.template(postList, {posts: this.posts.filterSourceBy(e.target.value)});
+			this.$('#posts-list').html(template);
 		}
-
 	});
 
 	return HomeView;
